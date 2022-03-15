8c31619ea841dd3b2f9dee03d2f5eaa52c9989fa
chhuti.mistry <chhuti246@gmail.com>
2022-03-15 11:01:39 +0530

    third change

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 function changemode(){
     var mybody = document.body;
     var mycouponbutton = document.getElementById("closecou");
+    var myservice = document.getElementById("myservice");
+    var myservicee = document.getElementById("myservicee");
+    var mmyservice = document.getElementById("mmyservice");
+    var myyservice = document.getElementById("myyservice");
+    var mysservice = document.getElementById("mysservice");
+    var myseervice = document.getElementById("myseervice");
+    var myserrvice = document.getElementById("myserrvice");
+    var mybio = document.getElementById("mybio");
+    var myreview = document.getElementById("myreview");
     mybody.classList.toggle(`mydark`);
     mycouponbutton.classList.toggle(`mycouponbut`);
+    myservice.classList.toggle(`myservic`);
+    myservicee.classList.toggle(`myservic`);
+    mmyservice.classList.toggle(`myservic`);
+    myyservice.classList.toggle(`myservic`);
+    mysservice.classList.toggle(`myservic`);
+    myseervice.classList.toggle(`myservic`);
+    myserrvice.classList.toggle(`myservic`);
+    mybio.classList.toggle(`myservic`);
+    myreview.classList.toggle(`myservic`);
 }
 
 function loadcoupon(){
